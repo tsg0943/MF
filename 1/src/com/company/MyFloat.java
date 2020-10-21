f5fe2ef6a76686e5a739b38da24c9f07fe7fd579
Ttsrr <tanya.grishina.02@gmail.com>
2020-10-21 11:34:03 +0300

    public class MyFloat

---
+++ 1/src/com/company/MyFloat.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class MyFloat {
+    private long mantissa;
+    private long exponent;
+
+    public MyFloat(long mantissa, long exponent) {
+        this.mantissa = mantissa;
+        this.exponent = exponent;
+
+    }
+}
