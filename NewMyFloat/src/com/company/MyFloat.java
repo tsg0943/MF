146e2093decbfd9f4e4a7d21c3c81866ba217dc0
Ttsrr <tanya.grishina.02@gmail.com>
2020-10-21 12:25:48 +0300

    Completion MyFloat

--- NewMyFloat/src/com/company/MyFloat.java
+++ NewMyFloat/src/com/company/MyFloat.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class MyFloat {
+    private long mantissa;
+    private long exponent;
+
+    public MyFloat(long mantissa, long exponent) {
+        this.mantissa = mantissa;
+        this.exponent = exponent;
+    }
 }
